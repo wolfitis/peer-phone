c313041a5457537bb8ab68a07a0e71c0516b1777
wolfitis <webblog123@gmail.com>
2021-01-18 04:15:40 -0500

    trying to bundle js files

--- script.js
+++ script.js
@@ -7,7 +7,11 @@
 * however, we absolutely need an ID to connect two peers together. Here' we're generating an ID. We also
 * need to pass some options such as the host of the peer, the debug level and the path to the where the peer code
 * is. */
+
+// module.exports = function () {
+// import { createRequire } from 'module';
 const Peer = require("peerjs");
+// import Peer from 'peerjs'
 
 const peer = new Peer('' + Math.floor(Math.random() * 2 ** 18).toString(36).padStart(4, 0), {
     host: location.hostname,
@@ -16,3 +20,24 @@ const peer = new Peer('' + Math.floor(Math.random() * 2 ** 18).toString(36).padS
 });
 
 window.peer = peer;
+
+function getLocalStream() {
+    navigator.mediaDevices.getUserMedia({ video: false, audio: true }).then(stream => {
+        window.localStream = stream;
+        window.localAudio.srcObject = stream;
+        window.localAudio.autoplay = true;
+    }).catch(err => {
+        console.log('There is an error: ' + err)
+    });
+}
+
+getLocalStream();
+
+// }
+
+
+
+
+
+// to run
+// peerjs --port 443 --key peerjs --path /myapp
